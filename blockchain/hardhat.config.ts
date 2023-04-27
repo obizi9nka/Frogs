5ccf0af09594a2d83f5f36ac9844d6d3e69cad5c
BigAnt <antropov.ssasha@yandex.ru>
2023-04-27 17:35:13 +0300

    fork

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -12,20 +12,20 @@ const config: HardhatUserConfig = {
     compilers: [
       {
         version: "0.8.1"
-      }
-      // {
-      //   version: "=0.7.6"
-      // }, {
-      //   version: "0.5.0"
-      // }, {
-      //   version: "^0.7.0"
-      // }, {
-      //   version: ">=0.7.5"
-      // }, {
-      //   version: "0.6.0"
-      // }, {
-      //   version: "0.7.6"
-      // },
+      },
+      {
+        version: "=0.7.6"
+      }, {
+        version: "0.5.0"
+      }, {
+        version: "^0.7.0"
+      }, {
+        version: ">=0.7.5"
+      }, {
+        version: "0.6.0"
+      }, {
+        version: "0.7.6"
+      },
     ],
     settings: {
       optimizer: {
